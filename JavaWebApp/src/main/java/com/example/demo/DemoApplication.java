193cfd485c233d7bf12ace9b4830e1cc96b4ed4b
“Goke <gokeplus@hotmail.com>
2023-11-01 01:01:06 -0500

    fixed syntax issue

--- JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -9,8 +9,9 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 
-		string name = "practice";
-		system.out.println(name);
+		String name = "practice";
+
+		System.out.println(name);
 	}
 
 }
